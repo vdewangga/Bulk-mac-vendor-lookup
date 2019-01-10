605ba30804a5dd5da9f5343ecdb60a80ee5e1262
vdewangga <vdewangga@users.noreply.github.com>
2019-01-10 20:03:59 +0700

    bulk_mac_find

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Bulk mac vendor lookup
+
+
+Kode ini ditulis dengan python 3.6
+Pengguna di harapkan terkoneksi internet dikarnakan tool ini menggunakan api dari :
+https://macvendors.co/api/
+
+Cara penggunaannya seperti di gambar berikut:
+<img src="images/contoh_penggunaan.PNG" alt="gambar_1">
+
+Penulisan mac address harus dengan spasi untuk pemisah bukan dengan enter:
+<img src="images/contoh_mac.PNG" alt="gambar_2">
---
+++ macfind.py
@@ -0,0 +1,27 @@
+import urllib.request
+import json
+import sys
+
+ar = sys.argv
+line = []
+v = {}
+file = open(str("contoh-mac.txt")).read().split()
+for m in range(len(file)):
+	j = json.load(urllib.request.urlopen('https://macvendors.co/api/'+file[m]))
+	v[file[m]] = j['result']['company']
+
+line.append(max(len(x) for x in v))
+line.append(max(len(v[x]) for x in v))
+
+lineA = "_" * (line[0]+5)
+lineB = "_" * (line[1]+2)
+
+print(lineA.strip() + lineB.strip() + "__".strip())
+print("|MAC Address         :" + "  vendor" + " " * (len(lineB) - len("vendor")) + "|")
+print("|" + lineA.strip() + lineB.strip() + "_".strip() + "|")
+
+for k, v in v.items():
+	spc = " " * (int(len(lineB)) - int(len(v)) - 2)
+	print("|", k, " : ", v, spc, "|")
+
+print("|" + lineA.strip() + lineB.strip() + "_".strip() + "|")
